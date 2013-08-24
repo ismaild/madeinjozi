a2910f6851a318088df348589fb7e97afefd15f6
Ismail Dhorat <ismail@zyelabs.net>
2013-08-24 11:15:29 +0200

    serve json

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Code for MadeinJozi Project @ tech4africa hackathon
+
+http://madeinjozi.herokuapp.com
+http://madeinjoziapi.herokuapp.com
--- app.js
+++ app.js
@@ -6,6 +6,17 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/json'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
